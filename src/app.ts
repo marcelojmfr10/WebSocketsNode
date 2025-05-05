d1138cff082d87761b5148f06b2d61a7d96a4edc
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-05-04 21:08:09 -0600

    Recibir datos del cliente

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
 
 
-console.log('hola mundo');
+import { WebSocketServer } from 'ws';
+
+//wss websocket server
+const wss = new WebSocketServer({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+    console.log('client connected');
+
+    ws.on('error', console.error);
+
+    ws.on('message', function message(data) {
+        console.log('desde el cliente', data);
+    });
+
+    ws.send('hola desde el servidor');
+
+    ws.on('close', () => {
+        console.log('client disconnected');
+    });
+});
+
+console.log('server running on port http://localhost:3000');
