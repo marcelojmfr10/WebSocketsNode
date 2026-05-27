2cee1ad5f5da46e5f4b9b0575ab59d289b15b69b
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2026-05-26 18:17:54 -0600

    Formatting

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,40 @@
-
-
-import { WebSocketServer, WebSocket } from 'ws';
+import { WebSocketServer, WebSocket } from "ws";
 
 //wss websocket server
 const wss = new WebSocketServer({ port: 3000 });
 
-wss.on('connection', function connection(ws) {
-    console.log('client connected');
+wss.on("connection", function connection(ws) {
+  console.log("client connected");
 
-    ws.on('error', console.error);
+  ws.on("error", console.error);
 
-    ws.on('message', function message(data) {
-        
-        const payload = JSON.stringify({
-            type: 'custom-message',
-            payload: data.toString(),
-        })
-        // ws.send(JSON.stringify(payload));
-
-        // * todos - incluyente
-        // wss.clients.forEach(function each(client) {
-        //     if (client.readyState === WebSocket.OPEN) {
-        //         client.send(payload, { binary: false });
-        //     }
-        // });
-
-        // * todos - excluyente
-        wss.clients.forEach(function each(client) {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(payload, { binary: false });
-            }
-        });
+  ws.on("message", function message(data) {
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString(),
     });
+    // ws.send(JSON.stringify(payload));
 
-    // ws.send('hola desde el servidor');
+    // * todos - incluyente
+    // wss.clients.forEach(function each(client) {
+    //     if (client.readyState === WebSocket.OPEN) {
+    //         client.send(payload, { binary: false });
+    //     }
+    // });
 
-    ws.on('close', () => {
-        console.log('client disconnected');
+    // * todos - excluyente
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
     });
+  });
+
+  // ws.send('hola desde el servidor');
+
+  ws.on("close", () => {
+    console.log("client disconnected");
+  });
 });
 
-console.log('server running on port http://localhost:3000');
+console.log("server running on port http://localhost:3000");
